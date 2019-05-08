e123a178ece1256ccbcf2770608cd5a8d2cc8e3d
Basavarajjangannavar <50196675+Basavarajjangannavar@users.noreply.github.com>
2019-05-08 15:50:26 +0530

    Update basu.c

--- basu.c
+++ basu.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
-void main() {
+void main() 
+{
 	int i,j,k,samp=1;
-	for (i=1; i<=5; i++) {
+	for (i=1; i<=5; i++) 
+	{
 		for (k=samp; k<=5; k++) {
 			printf(" ");
 		}
-		for (j=0; j< i; j++) {
+		for (j=0; j< i; j++)
+		{
 			printf("*");
 		}
 		samp = samp + 1;
 		printf("\n");
 	}
 	samp = 1;
-	for (i=4; i>=1; i--) {
-		for (k=samp; k>=0; k--) {
+	for (i=4; i>=1; i--) 
+	{
+		for (k=samp; k>=0; k--) 
+		{
 			printf(" ");
 		}
-		for (j=i; j>=1; j--) {
+		for (j=i; j>=1; j--) 
+		{
 			printf("*");
 		}
 		samp = samp + 1;
